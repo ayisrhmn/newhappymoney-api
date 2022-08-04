257a73ccab55f708d88c9d64a2ef59786832116e
Muhammad Fariz Rahman <muhammad.fariz@kanosolution.com>
2022-08-04 12:09:57 +0700

    add endpoint topspending report & add endpoint mybalance in transaction

--- app/report/router.js
+++ app/report/router.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
-const {spendingReport} = require('./controller');
+const {
+  spendingReport,
+  topIncomeSpending,
+  topExpenseSpending,
+} = require('./controller');
 const {isLoginUser} = require('../middleware/auth');
 
 router.post('/', isLoginUser, spendingReport);
+router.post('/top/income', isLoginUser, topIncomeSpending);
+router.post('/top/expense', isLoginUser, topExpenseSpending);
 
 module.exports = router;
--- app/transaction/controller.js
+++ app/transaction/controller.js
@@ -4,20 +4,60 @@ module.exports = {
   myTransaction: async (req, res) => {
     try {
       let User = req.user._id;
-      let {TrDateMonth} = req.body;
-      const checkDateMonth = TrDateMonth === undefined || TrDateMonth === '';
+      let {TrDateMonth, Show} = req.body;
 
-      const Data = await Transaction.find(
-        checkDateMonth ? {User} : {User, TrDateMonth},
-      )
+      const dtTransaction = await Transaction.find({User, TrDateMonth})
         .select('Title Note Category Amount TrDateMonth createdAt updatedAt')
         .populate('Category', 'Name Type Limit')
         .sort({createdAt: -1});
+
+      let Data = Show === 'Recent' ? dtTransaction.slice(0, 5) : dtTransaction;
 
       res.status(200).json({
         Success: true,
         Message: '',
         Data,
+      });
+    } catch (err) {
+      res.status(500).json({
+        Success: false,
+        Message: err.message || 'Internal server error!',
+      });
+    }
+  },
+  myBalance: async (req, res) => {
+    try {
+      let User = req.user._id;
+      let {TrDateMonth} = req.body;
+
+      // get all transaction
+      const getTransactions = await Transaction.find({
+        User,
+        TrDateMonth,
+      }).populate('Category');
+
+      // get transaction by income & calc total amount
+      let transactionByIncome = getTransactions.filter((item) => {
+        return item.Category.Type === 'Income';
+      });
+      let totalTrIncome = transactionByIncome.reduce((val, data) => {
+        return val + data.Amount;
+      }, 0);
+
+      // get transaction by expense & calc total amount
+      let transactionByExpense = getTransactions.filter((item) => {
+        return item.Category.Type === 'Expense';
+      });
+      let totalTrExpense = transactionByExpense.reduce((val, data) => {
+        return val + data.Amount;
+      }, 0);
+
+      let calcBalance = totalTrIncome - totalTrExpense;
+
+      res.status(200).json({
+        Success: true,
+        Message: '',
+        Data: calcBalance,
       });
     } catch (err) {
       res.status(500).json({
